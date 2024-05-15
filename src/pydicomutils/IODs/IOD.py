b9da4417766fd6c596b091e420a95b69daa937e5
Daniel Forsberg <daniel.forsberg@sectra.com>
2024-05-15 10:34:47 +0200

    Formatting changes

--- src/pydicomutils/IODs/IOD.py
+++ src/pydicomutils/IODs/IOD.py
@@ -2,27 +2,43 @@ import random
 from datetime import datetime
 from enum import Enum
 
-from pydicom import Dataset, FileDataset, DataElement, Sequence, write_file, read_file, uid
+from pydicom import (
+    Dataset,
+    FileDataset,
+    DataElement,
+    Sequence,
+    write_file,
+    read_file,
+    uid,
+)
 from pydicom.datadict import tag_for_keyword, dictionary_VR
 
 from .modules.general_modules import PatientModule, GeneralStudyModule
-from .modules.general_modules import GeneralSeriesModule, GeneralEquipmentModule, EnhancedGeneralEquipmentModule
+from .modules.general_modules import (
+    GeneralSeriesModule,
+    GeneralEquipmentModule,
+    EnhancedGeneralEquipmentModule,
+)
 from .modules.general_modules import GeneralImageModule, ImagePixelModule
 from .modules.general_modules import SOPCommonModule
 from .sequences.Sequences import generate_sequence
 
+
 class IODTypes(Enum):
-    """Definition of different IODs and their corresponding SOP Class UIDs
-    """
-    CRImage =     "1.2.840.10008.5.1.4.1.1.1"
-    CTImage =     "1.2.840.10008.5.1.4.1.1.2"
-    SCImage =     "1.2.840.10008.5.1.4.1.1.7"
-    GSPS =        "1.2.840.10008.5.1.4.1.1.11.1"
-    CSPS =        "1.2.840.10008.5.1.4.1.1.11.2"
+    """Definition of different IODs and their corresponding SOP Class UIDs"""
+
+    CRImage = "1.2.840.10008.5.1.4.1.1.1"
+    CTImage = "1.2.840.10008.5.1.4.1.1.2"
+    SCImage = "1.2.840.10008.5.1.4.1.1.7"
+    GSPS = "1.2.840.10008.5.1.4.1.1.11.1"
+    CSPS = "1.2.840.10008.5.1.4.1.1.11.2"
     BasicTextSR = "1.2.840.10008.5.1.4.1.1.88.11"
-    EnhancedSR =  "1.2.840.10008.5.1.4.1.1.88.22"
-    KOS =         "1.2.840.10008.5.1.4.1.1.88.59"
-    WSMImage =    "1.2.840.10008.5.1.4.1.1.77.1.6"
+    EnhancedSR = "1.2.840.10008.5.1.4.1.1.88.22"
+    ComprehensiveSR = "1.2.840.10008.5.1.4.1.1.88.33"
+    Comprehensive3DSR = "1.2.840.10008.5.1.4.1.1.88.34"
+    KOS = "1.2.840.10008.5.1.4.1.1.88.59"
+    WSMImage = "1.2.840.10008.5.1.4.1.1.77.1.6"
+
 
 """Dictionary to go from SOPClassUID to Modality code
 """
@@ -34,13 +50,16 @@ SOP_CLASS_UID_MODALITY_DICT = {
     IODTypes.CSPS: "PR",
     IODTypes.BasicTextSR: "SR",
     IODTypes.EnhancedSR: "SR",
+    IODTypes.ComprehensiveSR: "SR",
+    IODTypes.Comprehensive3DSR: "SR",
     IODTypes.KOS: "KO",
-    IODTypes.WSMImage: "SM"
+    IODTypes.WSMImage: "SM",
 }
 
+
 class IOD:
-    """Basic IOD class
-    """
+    """Basic IOD class"""
+
     iod_type = None
     dataset = None
 
@@ -76,84 +95,115 @@ class IOD:
         """
         if tag_for_keyword(keyword):
             if keyword in self.dataset:
-                if dictionary_VR(tag_for_keyword(keyword)) == "SQ" and isinstance(value, list):
+                if dictionary_VR(tag_for_keyword(keyword)) == "SQ" and isinstance(
+                    value, list
+                ):
                     value = generate_sequence(keyword, value)
                 self.dataset[tag_for_keyword(keyword)].value = value
             else:
-                if dictionary_VR(tag_for_keyword(keyword)) == "SQ" and isinstance(value, list):
+                if dictionary_VR(tag_for_keyword(keyword)) == "SQ" and isinstance(
+                    value, list
+                ):
                     value = generate_sequence(keyword, value)
-                de = DataElement(tag_for_keyword(keyword), 
-                                 dictionary_VR(tag_for_keyword(keyword)), 
-                                 value)
+                de = DataElement(
+                    tag_for_keyword(keyword),
+                    dictionary_VR(tag_for_keyword(keyword)),
+                    value,
+                )
                 self.dataset[tag_for_keyword(keyword)] = de
         else:
-            print("Keyword", keyword,"is an unknown DICOM attribute")
+            print("Keyword", keyword, "is an unknown DICOM attribute")
 
-    def copy_required_dicom_attributes(self, dataset_to_copy_from, 
-                                       include_optional=True):
+    def copy_required_dicom_attributes(
+        self, dataset_to_copy_from, include_optional=True
+    ):
         """Copies required DICOM attributes from provided dataset
         Parameters
         ----------
         dataset_to_copy_from : Dataset to copy DICOM attributes from
         include_optional : Include optional DICOM attributes in copy
         """
-        general_modules = [PatientModule(),
-                           GeneralStudyModule(),
-                           GeneralEquipmentModule(),
-                           SOPCommonModule()]
+        general_modules = [
+            PatientModule(),
+            GeneralStudyModule(),
+            GeneralEquipmentModule(),
+            SOPCommonModule(),
+        ]
         for module in general_modules:
-            module.copy_required_dicom_attributes(dataset_to_copy_from, 
-                                                  self.dataset)
+            module.copy_required_dicom_attributes(dataset_to_copy_from, self.dataset)
             if include_optional:
-                module.copy_optional_dicom_attributes(dataset_to_copy_from, 
-                                                      self.dataset)
+                module.copy_optional_dicom_attributes(
+                    dataset_to_copy_from, self.dataset
+                )
 
         if self.iod_type in [IODTypes.WSMImage]:
             general_modules = [EnhancedGeneralEquipmentModule()]
             for module in general_modules:
-                module.copy_required_dicom_attributes(dataset_to_copy_from, 
-                                                      self.dataset)
+                module.copy_required_dicom_attributes(
+                    dataset_to_copy_from, self.dataset
+                )
                 if include_optional:
-                    module.copy_optional_dicom_attributes(dataset_to_copy_from, 
-                                                          self.dataset)
+                    module.copy_optional_dicom_attributes(
+                        dataset_to_copy_from, self.dataset
+                    )
 
-        if self.iod_type in [IODTypes.CRImage, IODTypes.CTImage, IODTypes.SCImage, IODTypes.WSMImage]:
-            general_image_modules = [GeneralImageModule(),
-                                     GeneralSeriesModule(),
-                                     ImagePixelModule()]
+        if self.iod_type in [
+            IODTypes.CRImage,
+            IODTypes.CTImage,
+            IODTypes.SCImage,
+            IODTypes.WSMImage,
+        ]:
+            general_image_modules = [
+                GeneralImageModule(),
+                GeneralSeriesModule(),
+                ImagePixelModule(),
+            ]
             for module in general_image_modules:
-                module.copy_required_dicom_attributes(dataset_to_copy_from, 
-                                                      self.dataset)
+                module.copy_required_dicom_attributes(
+                    dataset_to_copy_from, self.dataset
+                )
                 if include_optional:
-                    module.copy_optional_dicom_attributes(dataset_to_copy_from, 
-                                                          self.dataset)
+                    module.copy_optional_dicom_attributes(
+                        dataset_to_copy_from, self.dataset
+                    )
 
         if self.iod_type in [IODTypes.GSPS, IODTypes.CSPS]:
             general_series_modules = [GeneralSeriesModule()]
             for module in general_series_modules:
-                module.copy_required_dicom_attributes(dataset_to_copy_from, 
-                                                    self.dataset)
+                module.copy_required_dicom_attributes(
+                    dataset_to_copy_from, self.dataset
+                )
                 if include_optional:
-                    module.copy_optional_dicom_attributes(dataset_to_copy_from, 
-                                                          self.dataset)
+                    module.copy_optional_dicom_attributes(
+                        dataset_to_copy_from, self.dataset
+                    )
 
     def initiate(self):
         """Initiate the IOD by setting some dummy values for
         required attributes
         """
         # patient module
-        self.dataset.PatientID = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+        self.dataset.PatientID = "".join(
+            random.choice("0123456789ABCDEF") for i in range(16)
+        )
         # general study module
         self.dataset.StudyInstanceUID = uid.generate_uid()
         self.dataset.StudyDate = datetime.now().strftime("%Y%m%d")
         self.dataset.StudyTime = datetime.now().strftime("%H%M%S")
-        self.dataset.StudyID = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+        self.dataset.StudyID = "".join(
+            random.choice("0123456789ABCDEF") for i in range(16)
+        )
         self.dataset.AccessionNumber = self.dataset.StudyID
         # sop common module
         self.dataset.SOPClassUID = self.iod_type.value
         self.dataset.SOPInstanceUID = uid.generate_uid()
 
-        if self.iod_type in [IODTypes.CRImage, IODTypes.CTImage, IODTypes.SCImage, IODTypes.WSMImage]:
+        if self.iod_type in [
+            IODTypes.CRImage,
+            IODTypes.CTImage,
+            IODTypes.SCImage,
+            IODTypes.WSMImage,
+        ]:
             # general series module
             self.dataset.Modality = SOP_CLASS_UID_MODALITY_DICT[self.iod_type]
             self.dataset.SeriesInstanceUID = uid.generate_uid()
@@ -168,8 +218,7 @@ class IOD:
             self.dataset.Modality = SOP_CLASS_UID_MODALITY_DICT[self.iod_type]
             self.dataset.SeriesInstanceUID = uid.generate_uid()
             self.dataset.SeriesNumber = str(100)
-            
-        
+
     def write_to_file(self, output_file, write_like_original=False):
         """Writes the current IOD to file
         Parameters
